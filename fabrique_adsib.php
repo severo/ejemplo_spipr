06259885c7bedb53237735d4e02c6e5fec70f22c
Sylvain Lesage <severo@rednegra.net>
2018-05-04 12:40:53 -0400

    Tema

---
+++ fabrique_adsib.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ *  Fichier généré par la Fabrique de plugin v6
+ *   le 2018-04-16 21:39:27
+ *
+ *  Ce fichier de sauvegarde peut servir à recréer
+ *  votre plugin avec le plugin «Fabrique» qui a servi à le créer.
+ *
+ *  Bien évidemment, les modifications apportées ultérieurement
+ *  par vos soins dans le code de ce plugin généré
+ *  NE SERONT PAS connues du plugin «Fabrique» et ne pourront pas
+ *  être recréées par lui !
+ *
+ *  La «Fabrique» ne pourra que régénerer le code de base du plugin
+ *  avec les informations dont il dispose.
+ *
+**/
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$data = array (
+  'fabrique' => 
+  array (
+    'version' => 6,
+  ),
+  'paquet' => 
+  array (
+    'prefixe' => 'adsib',
+    'nom' => 'ADSIB',
+    'slogan' => '',
+    'description' => '',
+    'logo' => 
+    array (
+      0 => '',
+    ),
+    'version' => '1.0.0',
+    'auteur' => 'Sylvain Lesage',
+    'auteur_lien' => '',
+    'licence' => 'GNU/GPL',
+    'categorie' => 'squelette',
+    'etat' => 'dev',
+    'compatibilite' => '[3.2.1;3.2.*]',
+    'documentation' => '',
+    'administrations' => '',
+    'schema' => '1.0.0',
+    'formulaire_config' => '',
+    'formulaire_config_titre' => '',
+    'inserer' => 
+    array (
+      'paquet' => '',
+      'administrations' => 
+      array (
+        'maj' => '',
+        'desinstallation' => '',
+        'fin' => '',
+      ),
+      'base' => 
+      array (
+        'tables' => 
+        array (
+          'fin' => '',
+        ),
+      ),
+    ),
+    'scripts' => 
+    array (
+      'pre_copie' => '',
+      'post_creation' => '',
+    ),
+    'exemples' => 'on',
+  ),
+  'objets' => 
+  array (
+  ),
+  'images' => 
+  array (
+    'paquet' => 
+    array (
+      'logo' => 
+      array (
+        0 => 
+        array (
+          'extension' => '',
+          'contenu' => '',
+        ),
+      ),
+    ),
+    'objets' => 
+    array (
+    ),
+  ),
+);
